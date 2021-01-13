6752c95505b339462e642c3897190433eee0305e
suyeon <sroovy@naver.com>
2021-01-13 22:39:07 +0900

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import ChatPage from './components/ChatPage/ChatPage'
+import LoginPage from './components/LoginPage/LoginPage'
+import RegisterPage from './components/RegisterPage/RegisterPage'
 
 function App() {
   return (
-    <div></div>
-
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ChatPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/resgister" component={RegisterPage} />
+      </Switch>
+    </Router>
   );
 }
 
