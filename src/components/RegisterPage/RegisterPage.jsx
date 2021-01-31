024a1131687e149ac461e02e998439ebb61fb7f2
suyeon <sroovy@naver.com>
2021-01-31 13:03:04 +0900

    react-hook-form으로 유효성 체크

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,45 +1,63 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 
 function RegisterPage() {
 
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => {
-    console.log(data);
-  };
+  const { register, watch, errors } = useForm({ mode: "onChange" });
+  const password = useRef();
+  password.current = watch("password");
 
   return (
     <div className="auth-wrapper">
       <div className="register-title">
         <h3>Register</h3>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit>
         <label>E-mail</label>
         <input 
           name="email" 
           type="email" 
-          defaultValue="test"
-          ref={register} />
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
+        {errors.email && <p>이메일을 입력해주세요. </p>}
+
         <label>Name</label>
         <input
           name="name"
           type="text"
           ref={register({ required: true, maxLength: 10 })}
         />
+        {errors.name && errors.name.type === "required"
+          && <p>이름을 입력해주세요.</p>}
+        {errors.name && errors.name.type === "maxLength"
+          && <p>이름을 10자 이내로 입력해주세요.</p>}
+
         <label>Password</label>
         <input
           name="password"
           type="password"
-          ref={register({ required: true, maxLength: 10 })}
+          ref={register({ required: true, minLength: 6 })}
         />
+        {errors.password && errors.password.type === "required"
+          && <p>패스워드를 입력해주세요.</p>}  {/* This password field is required */}
+        {errors.password && errors.password.type === "minLength"
+          && <p>패스워드를 6자 이상으로 입력해주세요.</p>} {/* Password must have at least 6 characters*/}
+
         <label>Password Confirm</label>
         <input
           name="password_confirm"
           type="password"
-          ref={register({ required: true, maxLength: 10 })}
+          ref={register({ 
+            required: true,  
+            validate: (value) => 
+              value === password.current
+          })}
         />
-        {errors.exampleRequired && <p>This field is required</p>}
+        {errors.password_confirm && errors.password_confirm.type === "required"
+          && <p>패스워드를 입력해주세요.</p>}  {/* This password field is required */}
+        {errors.password_confirm && errors.password_confirm.type === "validate"
+          && <p>패스워드가 같지 않습니다. </p>}  {/* The password don't match */}
+
         <button type="submit">SUBMIT</button>
       </form>
       <div className="link">
