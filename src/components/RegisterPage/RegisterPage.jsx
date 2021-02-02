779c91c8848dc0c133cf1fcb3286ce71d34ef8cb
suyeon <sroovy@naver.com>
2021-02-02 23:04:01 +0900

    Firebase에 생성한 유저를 데이터베이스에 저장
    * database()메소드를 사용하여 user의 정보를 저장한다. realtime이기 때문에 정보가 바로 저장된다.

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -35,7 +35,14 @@ function RegisterPage() {
           displayName: data.name,
           photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
         })
-        setLoading(false);
+        console.log("createdUser", createdUser);
+
+      // firebase 데이터 베이스에 저장해주기
+      await firebase.database().ref("users").child(createdUser.user.uid).set({
+        name: createdUser.user.displayName,
+        image: createdUser.user.photoURL
+      })
+      setLoading(false);
     } catch (error) {
         setErrorFromSubmit(error.message);
         setLoading(false);
