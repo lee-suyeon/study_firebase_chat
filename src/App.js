de755ef8882e37ee0889be83a167a3af3241b9e7
suyeon <sroovy@naver.com>
2021-02-05 20:51:42 +0900

    인증된 이후의 페이지 이동 & useHistory
    * history객체에 접근하기 위해 useHistory hooks를 사용한다.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useHistory,
 } from "react-router-dom";
 
 import ChatPage from './components/ChatPage/ChatPage'
 import LoginPage from './components/LoginPage/LoginPage'
 import RegisterPage from './components/RegisterPage/RegisterPage'
 
-function App() {
+import firebase from './firebase';
+
+function App(props) {
+  let history = useHistory();
+  
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log("user", user);
+      // 로그인이 된 상태
+      if(user){
+        history.push("/");
+      } else { // 로그인이 안 된 상태
+        history.push("/login");
+      }
+    });
+  }, [])
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ChatPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/register" component={RegisterPage} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={ChatPage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/register" component={RegisterPage} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReuduxThunk from 'redux-thunk';
 import Reducer from './redux/reducer';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -18,8 +21,10 @@ ReactDOM.render(
     <Provider store={createStoreWithMiddleware(Reducer,
       window.__REDUX_DEVTOOLS_EXTENSION__&&
       window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}>
-      <App />
+      )}>
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
