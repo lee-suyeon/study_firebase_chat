96d923e6993ff140e63cf3360fc884bd756e15f7
suyeon <sroovy@naver.com>
2021-03-02 22:06:03 +0900

    Message Body, Message Form UI

--- src/components/ChatPage/MainPanel/MessageForm.jsx
+++ src/components/ChatPage/MainPanel/MessageForm.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import { Form, ProgressBar, Row, Col } from 'react-bootstrap'
+
+const StyledButton = styled.button`
+  background: #7ebeb6;
+  width: 100%;
+  color: white;
+  text-transform: uppercase;
+  border: none;
+  margin-top: 30px;
+  padding: 18px;
+  font-size: 16px;
+  font-weight: 200;
+  letter-spacing: 10px;
+
+  &:hover {
+    background: #509a8f;
+  }
+`
 
 function MessageForm() {
   return (
-    <div>MessageForm</div>
+    <div>
+      <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      
+      <ProgressBar 
+        variant="warning" 
+        label="60%"
+        now={60} 
+      /> 
+
+      <Row>
+        <Col>
+          <StyledButton>SEND</StyledButton>
+        </Col>
+        <Col>
+          <StyledButton>UPLOAD</StyledButton>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
