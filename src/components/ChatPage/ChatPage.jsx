beaaa51c23b2817c34a10cb8296221cdc932a359
suyeon <sroovy@naver.com>
2021-03-04 22:25:17 +0900

    Add Message Component

--- src/components/ChatPage/ChatPage.jsx
+++ src/components/ChatPage/ChatPage.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import SidePanel from './SidePanel/SidePanel';
 import MainPanel from './MainPanel/MainPanel';
+import { useSelector } from 'react-redux';
 
 function ChatPage() {
+
+  const currentChatRoom = useSelector(state => state.chatRoom.currentChatRoom);
+
   return (
     <div style={{ display: 'flex'}}>
       <div style={{ width: '300px' }}>
         <SidePanel />
       </div>
       <div style={{ width: '100%' }}>
-        <MainPanel />
+        <MainPanel 
+          key={currentChatRoom && currentChatRoom.id}
+        />
       </div>
     </div>
   )
--- src/components/ChatPage/MainPanel/Message.jsx
+++ src/components/ChatPage/MainPanel/Message.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import styled from 'styled-components'
+import moment from 'moment'
 
-function Message() {
+import { Media } from 'react-bootstrap'
+
+const MessageImage = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 10px;
+`
+
+const Timestamp = styled.span`
+  font-size: 10px;
+  color: gray;
+`
+
+function Message({ message, user }) {
+
+  const timeFromNow = timestamp => moment(timestamp).fromNow();
+  
+  const isImage = message => {
+    return message.hasOwnProperty("image") && !message.hasOwnProperty("content");
+  }
+
+  const isMessageMine = (message, user) => {
+    return message.user.id === user.id
+  }
+
   return (
-    <div>Message</div>
+    <Media style={{ marginBottom: '3px' }}>
+      <MessageImage
+        src={message.user.image}
+        alt={message.user.name}
+      />
+      <Media.Body style={{ backgroundColor: isMessageMine(message, user) && "#ececec" }}>
+        <h6>{message.user.name}{" "}
+          <Timestamp>
+            {timeFromNow(message.timestamp)}
+          </Timestamp>
+        </h6>
+          {isImage(message) ? 
+            <img 
+              style={{ maxWidth: '300px'}} 
+              alt="이미지" 
+              src={message.image} 
+            /> :
+            <p>{message.content}</p>
+          }
+      </Media.Body>
+    </Media>
   )
 }
 
