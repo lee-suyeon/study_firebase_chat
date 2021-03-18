76573b3ddc39ebbf0e4a118a35fdae860355f988
suyeon <sroovy@naver.com>
2021-03-18 21:46:16 +0900

    Direct Message - 모든 user 데이터 가져오기

--- src/components/ChatPage/SidePanel/DirectMessages.jsx
+++ src/components/ChatPage/SidePanel/DirectMessages.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
-function DirectMessages() {
-  return (
-    <div>DirectMessages</div>
-  )
+import { FiSmile } from 'react-icons/fi'
+import firebase from '../../../firebase'
+import { connect } from 'react-redux'
+
+const DirectWrapper = styled.div`
+
+`
+
+const DirectList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`
+
+export class DirectMessages extends Component {
+
+  state = {
+    usersRef: firebase.database().ref("users")
+  }
+
+  componentDidMount() {
+    const { user } = this.props;
+
+    this.addUsersListeners(user.uid)
+  }
+
+  addUsersListeners = (currentUserId) => {
+    const { usersRef } = this.state;
+
+    let usersArray = [];
+
+    usersRef.on("child_added", DataSnapshot => {
+      if(currentUserId !== DataSnapshot.key) {
+        let user = DataSnapshot.val()
+        user["uid"] = DataSnapshot.key;
+        user["status"] = "offline";
+        usersArray.push(user)
+        this.setState({ users: usersArray })
+      }
+    })
+  }
+
+  
+  renderDirectMessages = () => {
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        <DirectWrapper>
+          <FiSmile /> DIRECT MESSAGES(1)
+        </DirectWrapper>
+
+        <DirectList>
+          {this.renderDirectMessages()}
+        </DirectList>
+      </div>
+    )
+  }
 }
 
-export default DirectMessages
+const mapStateToProps = state => {
+  return {
+    user: state.user.currentUser
+  }
+}
+
+
+export default connect(mapStateToProps)(DirectMessages)
